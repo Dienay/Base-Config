23e7e51f986cf81c398835c94b0765ca23894a30
Dienay Lima <dienaylima@gmail.com>
2024-02-07 09:42:25 -0300

    Atualização dos apps que uso

--- initial-install.sh
+++ initial-install.sh
@@ -13,18 +13,25 @@ sudo pacman -S yay --noconfirm
 APPS=(
     "--needed base-devel git"
     "google-chrome"
-    "visual-studio-code-bin sublime-text-dev sublime-merge"
-    "blender inkscape gimp"
+    "visual-studio-code-bin sublime-text-dev sublime-merge intellij-idea-community-edition"
+    "blender inkscape gimp freecad krita"
     "discord telegram-desktop slack-desktop"
     "vlc audacity"
     "pyenv python-pip"
     "docker docker-compose"
+    "1password"
 )
-
+ 
 # Instalando APPS
 for app in "${APPS[@]}"; do
   yay -S $app --noconfirm
 done
 
-echo "Instalação concluída."
+# Instala o Docker Desktop
+cd /tmp
+wget -O docker-desktop.pkg.tar.zst https://desktop.docker.com/linux/main/amd64/136059/docker-desktop-4.27.1-x86_64.pkg.tar.zst
+sudo pacman -U ./docker-desktop.pkg.tar.zst
+systemctl --user start docker-desktop
+cd ~
 
+echo "Instalação concluída."
