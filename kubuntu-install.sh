e836272e2d591c5059d1b4da8a1e700a587faed7
Dienay Lima <dienaylima@gmail.com>
2024-12-29 11:34:18 -0300

    update

--- kubuntu-install.sh
+++ kubuntu-install.sh
@@ -11,30 +11,21 @@ APPS=(
     "gdebi"
     "synaptic"
     "flatpak"
-    "gnome-software"
-    "chrome-gnome-shell"
-    "gnome-shell-extensions"
-    "gnome-software-plugin-flatpak"
-    "gnome-tweaks"
-    "tlp"
-    "tlp-rdw"
     "audacious"
     "vlc"
-    "kodi"
     "audacity"
     "handbrake"
     "inkscape"
     "stacer"
-    "nautilus-megasync"
     "uget"
     "qbittorrent"
     "gparted"
     "git"
     "tilix"
-    "mysql-server"
-    "gnupg"
     "curl"
     "zsh"
+    "asdf"
+    "nala"
 )
 
 # Instala os pacotes
@@ -42,18 +33,8 @@ for app in "${APPS[@]}"; do
     sudo apt install -y $app
 done
 
-# ativa mysql server
-systemctl is-active mysql
-
-# habilita tlp
-systemctl enable tlp && tlp start
-
 # habilita flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# instala megasync
-cd /tmp && wget -O megasync.deb 'https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb'
-sudo apt install ./megasync.deb && cd ..
 
 FLATPACK=(
     "flathub org.chromium.Chromium"
@@ -86,11 +67,5 @@ sudo dpkg -i mongodb-compass.deb
 
 # instala mysql workbench
 sudo snap install mysql-workbench-community
-
-# instala nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
-
-# habilita minimizar janela com click no incone do aplicativo na dock
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
 echo "Instalação concluída."
